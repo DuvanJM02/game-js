ff0959e400d14864a24f05a0134303d3cc23e082
Duván Jaramillo <duvanlol02@gmail.com>
2022-01-13 14:40:55 -0500

    a

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hola mundo</h1>
-</body>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Juego web">
+        <meta name="author" content="ryokk">
+        <meta name="contact" content="djaramillom0r@gmail.com">
+        <title>Juego web</title>
+    </head>
+    <body>
+        <h1>Hola mundo</h1>
+        <script src="js/buclePrincipal.js"></script>
+        <script src="js/inicio.js"></script>
+    </body>
 </html>
